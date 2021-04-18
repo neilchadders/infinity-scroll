cf480fc416a296fee40a4abd67507c0f57aca056
Neil Chadwick <neilchadders@hotmail.com>
2021-04-18 17:47:57 +0100

    adding api key

---
+++ index.js
@@ -0,0 +1,6 @@
+//Unsplash API
+
+const count = 10;
+const apiKey = "EqeYLt5fh-YwodIaii3vCeL1DmvR25W6cX_hm0_8n10";
+
+const apiURL = "https://api.unsplash.com/photos/random/?client_id=${apiKey}YOUR_ACCESS_KEY&count=${count}";
